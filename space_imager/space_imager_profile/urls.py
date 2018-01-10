9987d5ad7c56284012478846a92f2369facc0672
David Franklin <dave5801@gmail.com>
2018-01-10 14:22:23 -0800

    setting up urls

--- space_imager/space_imager_profile/urls.py
+++ space_imager/space_imager_profile/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+'''
 urlpatterns = [
     path('', views.home_view, name='home_view'),
+]
+
+'''
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
 ]
