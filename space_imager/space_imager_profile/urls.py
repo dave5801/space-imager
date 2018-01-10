e60fe2e9c2ee0ce0a285c3f58be5c858d02ee5f5
David Franklin <dave5801@gmail.com>
2018-01-10 14:38:08 -0800

    urls in place

--- space_imager/space_imager_profile/urls.py
+++ space_imager/space_imager_profile/urls.py
@@ -2,13 +2,8 @@ from django.urls import path
 
 from . import views
 
-'''
 urlpatterns = [
     path('', views.home_view, name='home_view'),
-]
-
-'''
-
-urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
 ]
