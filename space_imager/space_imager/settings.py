5b86349987e34f6824fef5ff45e6b779177d4dc4
David Franklin <dave5801@gmail.com>
2018-01-23 21:39:28 -0800

    security update

--- space_imager/space_imager/settings.py
+++ space_imager/space_imager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nhn0qf7a0f#-)atxlu0t8d&g$j=(=s6u_koum^0iq*ubte%j-#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
