41ac6b33a0f12e0f76512734a737c2af9e7a2ea4
David Franklin <dave5801@gmail.com>
2018-01-25 11:32:42 -0800

    test still not working but need to switch gears

--- space_imager/space_imager_profile/tests.py
+++ space_imager/space_imager_profile/tests.py
@@ -7,5 +7,8 @@ class TestNASA_API_Call_and_json_response(TestCase):
 
     def test_json_response_is_correct(self):
         test_get_newly_created_photo = Create_A_New_Space_Photo_from_NASA_API()
-        assert self.assertIsNotNone(test_get_newly_created_photo)
+        test_new_space_photo = test_get_newly_created_photo.create_space_photo_from_nasa_api()
+        print("TESTING")
+        print(test_new_space_photo)
+        assert self.assertIsNone(test_new_space_photo)
 
