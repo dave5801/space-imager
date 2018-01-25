16d45be477c01463ff1fdf8f735645a9fca7db5e
David Franklin <dave5801@gmail.com>
2018-01-25 11:15:30 -0800

    wrote a test

--- space_imager/space_imager_profile/tests.py
+++ space_imager/space_imager_profile/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 
-from models import 
+from space_imager_profile.models import Create_A_New_Space_Photo_from_NASA_API
 
-# Create your tests here.
+
 class TestNASA_API_Call_and_json_response(TestCase):
 
     def test_json_response_is_correct(self):
+        test_get_newly_created_photo = Create_A_New_Space_Photo_from_NASA_API()
+        assert self.assertIsNotNone(test_get_newly_created_photo)
+
