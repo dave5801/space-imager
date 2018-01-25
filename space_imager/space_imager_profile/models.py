178babe5b60a7e195e0db1a950e98349f7932387
David Franklin <dave5801@gmail.com>
2018-01-25 10:22:31 -0800

    starting tests, refactor of models

--- space_imager/space_imager_profile/models.py
+++ space_imager/space_imager_profile/models.py
@@ -4,30 +4,29 @@ from django.db import models
 import os
 import requests
 
-class SpacePhotoManager(models.Manager):
-    def create_space_photo(self):
-        nasa_apod_url = os.environ.get('NASA_APOD_URL', '')
-        nasa_api_key = os.environ.get('NASA_API_KEY', '')
+class Create_A_New_Space_Photo_from_NASA_API(models.Manager):
+    def create_space_photo_from_nasa_api(self):
+        nasa_apod_url_from_environment_variables = os.environ.get('NASA_APOD_URL', '')
+        nasa_api_key_from_environment_variables = os.environ.get('NASA_API_KEY', '')
 
-        make_request_from_nasa_api = requests.get(nasa_apod_url + nasa_api_key)
-        nasa_request_converted_to_json = make_request_from_nasa_api.json()
+        get_response_from_nasa_api_request = requests.get(nasa_apod_url_from_environment_variables
+         + nasa_api_key_from_environment_variables)
 
-        '''Note - copyright field not in every NASA photo - revisit to later'''
-        space_photo = self.create(
-            #copyright=nasa_request_converted_to_json['copyright'],
-            date=nasa_request_converted_to_json['date'],
-            explanation=nasa_request_converted_to_json['explanation'],
-            hdurl=nasa_request_converted_to_json['hdurl'],
-            media_type=nasa_request_converted_to_json['media_type'],
-            service_version=nasa_request_converted_to_json['service_version'],
-            title=nasa_request_converted_to_json['title'],
-            url=nasa_request_converted_to_json['url'] 
+        get_response_from_nasa_api_request_converted_to_json = get_response_from_nasa_api_request.json()
+
+        a_new_space_photo = self.create(
+            date=get_response_from_nasa_api_request_converted_to_json['date'],
+            explanation=get_response_from_nasa_api_request_converted_to_json['explanation'],
+            hdurl=get_response_from_nasa_api_request_converted_to_json['hdurl'],
+            media_type=get_response_from_nasa_api_request_converted_to_json['media_type'],
+            service_version=get_response_from_nasa_api_request_converted_to_json['service_version'],
+            title=get_response_from_nasa_api_request_converted_to_json['title'],
+            url=get_response_from_nasa_api_request_converted_to_json['url'] 
             )
 
-        return space_photo
+        return a_new_space_photo
  
-class SpacePhoto(models.Model): 
-    #copyright = models.TextField(max_length=2000, null=True)
+class Space_Photo_From_NASA(models.Model): 
     date = models.DateTimeField(auto_now=True)
     explanation = models.TextField(max_length=2000, null=True)
     hdurl = models.CharField(max_length=180, null=True)
@@ -36,5 +35,5 @@ class SpacePhoto(models.Model):
     title = models.TextField(max_length=50)
     url = models.CharField(max_length=180, null=True)
 
-    objects = SpacePhotoManager()
+    get_newly_created_photo = Create_A_New_Space_Photo_from_NASA_API()
 
--- space_imager/space_imager_profile/tests.py
+++ space_imager/space_imager_profile/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+from models import 
+
 # Create your tests here.
+class TestNASA_API_Call_and_json_response(TestCase):
+
+    def test_json_response_is_correct(self):
