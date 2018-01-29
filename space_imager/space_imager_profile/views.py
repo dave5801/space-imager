33ed7e12f94885fac47c2e32e8d6799e8f31db39
David Franklin <dave5801@gmail.com>
2018-01-29 15:30:02 -0800

    calling new interface from views

--- space_imager/space_imager_profile/views.py
+++ space_imager/space_imager_profile/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from space_imager_profile.models import Space_Photo_From_NASA
+from space_imager_profile.nasa_api_interface import NASASpacePhotoAPICallInterface
 
 
 def home_view(request):
     """Home view callable, for the home page."""
-    new_space_photo = Space_Photo_From_NASA.get_newly_created_photo.create_space_photo_from_nasa_api()
-    print(new_space_photo.explanation)
+    new_space_photo = NASASpacePhotoAPICallInterface()
+    print(new_space_photo.get_response_from_nasa_api_request_converted_to_json)
 
     template = loader.get_template("space_imager_profile/home.html")
     response_body = template.render()
