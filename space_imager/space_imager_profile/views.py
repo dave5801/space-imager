d35357fb545500d301d8882e87d87878d396fe40
David Franklin <dave5801@gmail.com>
2018-01-29 16:11:07 -0800

    maybe the model is working?

--- space_imager/space_imager_profile/views.py
+++ space_imager/space_imager_profile/views.py
@@ -6,8 +6,11 @@ from space_imager_profile.nasa_api_interface import NASASpacePhotoAPICallInterfa
 
 def home_view(request):
     """Home view callable, for the home page."""
-    new_space_photo = NASASpacePhotoAPICallInterface()
-    print(new_space_photo.get_response_from_nasa_api_request_converted_to_json)
+    space_photo_interface = NASASpacePhotoAPICallInterface()
+   # print(space_photo_interface.get_response_from_nasa_api_request_converted_to_json)
+    #reg_face_verifier = FaceVerificationManager.objects.first()
+    new_space_photo = Space_Photo_From_NASA.objects.first()
+    print(new_space_photo)
 
     template = loader.get_template("space_imager_profile/home.html")
     response_body = template.render()
